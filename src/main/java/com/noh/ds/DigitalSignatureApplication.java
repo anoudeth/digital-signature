a6c35c80d9b0e2dc7f039dc0d7c87b5887e898de
nohder <pisces2531@gmail.com>
2022-03-26 16:12:43 +0700

    first

---
+++ src/main/java/com/noh/ds/DigitalSignatureApplication.java
@@ -0,0 +1,13 @@
+package com.noh.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignatureApplication.class, args);
+	}
+
+}
